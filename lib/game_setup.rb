cd493d91c25af19795699b4be4cc8cea5c08727b
Tyler Decker <tyler.decker@gmail.com>
2016-05-11 16:13:00 -0500

    Changed GameSetup to compare input to integers

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -1,10 +1,10 @@
 class GameSetup
 
   OPPONENT_TEXT = "\nSelect an Opponent to Play against.\n1. Human\n2. Ai Easy\n3. Ai Hard\n:"
-  OPPONENT_CHOICES = ["1", "2", "3"]
+  OPPONENT_CHOICES = [1, 2, 3]
 
   ORDER_TEXT = "\nWho should got first.\n1. Player\n2. Opponent\n:"
-  ORDER_CHOICES = ["1", "2"]
+  ORDER_CHOICES = [1, 2]
 
   def initialize(game_screen)
     @screen = game_screen
@@ -13,25 +13,27 @@ class GameSetup
   def setup
     order = get_order
     opponent = get_opponent
-    order == "1" ? ["HUMAN", opponent] : [opponent, "HUMAN"]
+    order == 1 ? ["HUMAN", opponent] : [opponent, "HUMAN"]
   end
 
   private
 
   def get_order
+    @screen.clear
     @screen.display_message(ORDER_TEXT)
     @screen.get_input(ORDER_CHOICES)
   end
 
   def get_opponent
+    @screen.clear
     @screen.display_message(OPPONENT_TEXT)
     choice = @screen.get_input(OPPONENT_CHOICES)
     case choice
-    when "1"
+    when 1
       opponent = "HUMAN"
-    when "2"
+    when 2
       opponent = "EASY"
-    when "3"
+    when 3
       opponent = "HARD"
     end
     opponent
--- spec/game_setup_spec.rb
+++ spec/game_setup_spec.rb
@@ -7,15 +7,17 @@ describe GameSetup do
   describe "setting up the game" do
     it "setup method returns HUMAN HUMAN when input is 1 and 1" do
       allow(screen).to receive(:display_message)
-      expect(screen).to receive(:get_input).and_return("1", "1")
+      allow(screen).to receive(:clear)
+      expect(screen).to receive(:get_input).and_return(1, 1)
       player1, player2 = setup.setup
       expect(player1).to eq("HUMAN")
       expect(player2).to eq("HUMAN")
-    end      
+    end
 
     it "setup method returns HUMAN EASY when input is 1 and 2" do
       allow(screen).to receive(:display_message)
-      expect(screen).to receive(:get_input).and_return("1", "2")
+      allow(screen).to receive(:clear)
+      expect(screen).to receive(:get_input).and_return(1, 2)
       player1, player2 = setup.setup
       expect(player1).to eq("HUMAN")
       expect(player2).to eq("EASY")
@@ -23,19 +25,20 @@ describe GameSetup do
 
     it "setup method returns HUMAN HARD when input is 1 and 3" do
       allow(screen).to receive(:display_message)
-      expect(screen).to receive(:get_input).and_return("1", "3")
+      allow(screen).to receive(:clear)
+      expect(screen).to receive(:get_input).and_return(1, 3)
       player1, player2 = setup.setup
       expect(player1).to eq("HUMAN")
       expect(player2).to eq("HARD")
     end
 
-     it "setup method returns HARD HUMAN when input is 2 and 3" do
+    it "setup method returns HARD HUMAN when input is 2 and 3" do
       allow(screen).to receive(:display_message)
-      expect(screen).to receive(:get_input).and_return("2", "3")
+      allow(screen).to receive(:clear)
+      expect(screen).to receive(:get_input).and_return(2, 3)
       player1, player2 = setup.setup
       expect(player1).to eq("HARD")
       expect(player2).to eq("HUMAN")
     end
-    
   end
 end
