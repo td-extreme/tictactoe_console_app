2239176762b3f252398b31f2f7b12506e18d6065
Tyler Decker <tyler.decker@gmail.com>
2016-05-31 13:49:17 -0500

    Made it so other classes are injected into main.

--- lib/terminal_tictactoe.rb
+++ lib/terminal_tictactoe.rb
@@ -1,31 +1,23 @@
-require 'tictactoe'
-require 'game_screen'
-require 'game_loop'
-require 'game_setup'
-require 'wrapper_stdio'
-
 class TerminalTicTacToe
 
-  def initialize
-    @screen = GameScreen.new(WrapperStdio.new)
+  def initialize(game, game_loop, game_screen)
+    @game = game
+    @game_loop = game_loop
+    @game_screen = game_screen 
   end
 
   def play_game
-    game_loop = GameLoop.new(@game, @screen)
     game_loop.play
     end_of_game
   end
 
   def end_of_game
-    @screen.clear
-    @screen.display_title
-    @screen.display_board(@game.get_board)
-    @game.tied_game? ? @screen.display_tied : @screen.display_winner(@game.winner) 
+    game_screen.clear
+    game_screen.display_title
+    game_screen.display_board(game.get_board)
+    game.tied_game? ? game_screen.display_tied : game_screen.display_winner(game.winner) 
   end
 
-  def setup
-    game_setup = GameSetup.new(@screen)
-    @player1, @player2 = game_setup.setup
-    @game = TicTacToe.new(@player1, @player2)
-  end
+  private
+  attr_accessor :game, :game_loop, :game_screen
 end
