ba08e3b4a9620008e20d7a004f923c9e84f76ad0
Tyler Decker <tyler.decker@gmail.com>
2016-06-02 09:56:11 -0500

    Added before to remove duplication.

--- spec/game_loop_spec.rb
+++ spec/game_loop_spec.rb
@@ -6,26 +6,23 @@ describe GameLoop do
   let (:player_manager) { double }
   let (:test_loop) { GameLoop.new(game, screen, player_manager) }
 
+  before(:each) do
+    allow(screen).to receive(:clear)
+    allow(screen).to receive(:display_title)
+    allow(screen).to receive(:display_board)
+    allow(screen).to receive(:display_message)
+    allow(screen).to receive(:display_key)
+    allow(game).to receive(:get_board)
+    allow(test_loop).to receive(:play_move)
+  end
+
   describe "The game loop" do
     it "the loop exits if the game is over" do
-      allow(screen).to receive(:clear)
-      allow(screen).to receive(:display_title)
-      allow(screen).to receive(:display_board)
-      allow(screen).to receive(:display_message)
-      allow(screen).to receive(:display_key)
-      allow(game).to receive(:get_board)
-      allow(test_loop).to receive(:play_move)
       expect(game).to receive(:game_over?).and_return(true)
       test_loop.play
     end
 
     it "the loop doesn't exit when the game is still going" do
-      allow(screen).to receive(:clear)
-      allow(screen).to receive(:display_title)
-      allow(screen).to receive(:display_board)
-      allow(screen).to receive(:display_message)
-      allow(screen).to receive(:display_key)
-      allow(game).to receive(:get_board)
       expect(test_loop).to receive(:play_move).twice
       expect(game).to receive(:game_over?).and_return(false, true)
       test_loop.play
--- spec/terminal_tictactoe_spec.rb
+++ spec/terminal_tictactoe_spec.rb
@@ -6,23 +6,22 @@ describe TerminalTicTacToe do
   let(:game_loop) { double }
   let(:ttt_test) { TerminalTicTacToe.new(game, game_loop, screen) }
 
+  before(:each) do
+    allow(screen).to receive(:clear)
+    allow(screen).to receive(:display_title)
+    allow(screen).to receive(:display_board)
+    allow(game).to receive(:get_board)
+    allow(game).to receive(:winner)
+  end
+
   describe "end of game"  do
     it "calls display_tied when game is tied" do
-      allow(screen).to receive(:clear)
-      allow(screen).to receive(:display_title)
-      allow(screen).to receive(:display_board)
-      allow(game).to receive(:get_board)
       expect(screen).to receive(:display_tied)
       expect(game).to receive(:tied_game?).and_return(true)
       ttt_test.end_of_game
     end
 
     it "calls display_winner when game is tied" do
-      allow(screen).to receive(:clear)
-      allow(screen).to receive(:display_title)
-      allow(screen).to receive(:display_board)
-      allow(game).to receive(:get_board)
-      allow(game).to receive(:winner)
       expect(screen).to receive(:display_winner)
       expect(game).to receive(:tied_game?).and_return(false)
       ttt_test.end_of_game
